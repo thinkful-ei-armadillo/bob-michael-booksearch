8b4c42f0ec135898f31cf65162d690f63a44bd9c
Michael Bramble <mike.w.bramble@gmail.com>
2019-02-11 15:28:15 -0500

    search funtionality working

--- src/App.js
+++ src/App.js
@@ -3,33 +3,39 @@ import Header from './Header'
 import BookList from './BookList' 
 //API_KEY = 'AIzaSyCMf97A53S_uz02WiwQPQQRmq83l-Y0UqY'
 
-
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      search: 'henry',
-      printType: '',
+      search: '',
+      printType: 'all',
       bookType: ''
     }
   }
 
+  handleSearch = query =>{
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.search}?printType=${this.state.printType}?filter=${this.state.bookType}`)
+      .then(res => res.json())
+      .then(books => this.setState({
+        search: query,
+        books: books.items
+      }));
+  }
+
   componentDidMount() {
-    this.setState({ loading: true }); // AJAX request is starting
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.search}`)
       .then(res => res.json())
       .then(books => this.setState({
-        books: books.items,
+        books: books.items
       }));
   }
 
   render(){
     return (
       <div>
-        <Header />
-        <BookList books = {this.state.books}/>
+        <Header handleSearch={this.handleSearch}/>
+        {(!this.state.search) ? '' : <BookList books={this.state.books}/>}
       </div>
     );
   }
--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react'
-//title = author = description = price = 
+import React, {Component} from 'react';
 
 class Book extends Component {
 
