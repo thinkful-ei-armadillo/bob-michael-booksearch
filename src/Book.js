d876e37bd7f43ac7106559bc80aedd22bd4ab990
bobnearents <bobnearents@yahoo.com>
2019-02-11 16:51:09 -0800

    fixed search for books w no authors

--- src/Book.js
+++ src/Book.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 class Book extends Component {
 
   render() {
+    const author = (this.props.author) ? this.props.author.join(', ') : '';
     return (
       <li>
         <img src={this.props.image} alt="cover" />
         <h2>{this.props.title}</h2>
-        <h3>{this.props.author.join(', ')}</h3>
+        <h3>{author}</h3>
         <h4><i>{this.props.price}</i></h4>
         <p>{this.props.description}</p>
       </li>
